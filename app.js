51bc58b6133ff0ec74f02e1e128efefd1c33846a
Roberto Mazzalai <71696642+robyconlay-uni@users.noreply.github.com>
2020-11-03 11:06:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
 const express = require('express');
 const app = express();
 
-const locations = require('.api/routes/locations');
+const locationsRoutes = require('.api/routes/locations');
 
 
-app.use('/locations', locations);
+app.use('/locations', locationsRoutes);
 
-app.use((req, res, next) => {
-    const error = new Error('Not Found');
-    error.staus = 404;
-    next(error);
+app.use((req, res) => {
+    res.status(404);
+    res.json({error : 'Not Found'});
 });
 
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error : {
-            message : error.message
-        }
-    })
-});
 
 module.exports = app;
