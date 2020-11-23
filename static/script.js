1f7ee4f508c628f76774a13efbf29aa07c7b2115
MarcoCarraro-1 <71838678+MarcoCarraro-1@users.noreply.github.com>
2020-11-23 16:02:57 +0100

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,8 +1,32 @@
 //here goes the script for the html page
 
-function login() {
+function loadLocations() {
 
+    const ul = document.getElementById("locations"); 
+    ul.innerHTML = '';
 
-    //use fetch function
+    fetch('../locations')
+    .then((resp) => resp.json()) // Transform the data into json
+    .then(function(data) { // Here you get the data to modify as you please
+        
+        // console.log(data);
+        
+        return data.map(function(location) { // Map through the results and for each run the code below
+                   
+            let li = document.createElement('li');
+            let span = document.createElement('span');
+            span.innerHTML = `<a href="${location.id}">${location.name}</a>`;
+            span.innerHTML += `<button type="button" onclick="openLocation('${location._id}')">Visualizza luogo</button>`
+            
+            // Append all our elements
+            li.appendChild(span);
+            ul.appendChild(li);
+        })
+    })
+    .catch( error => console.error(error) );// If there is any error you will catch them here
+    
+}
 
+function openLocation(idLocation){
+    window.open("localhost:8000/locations/"+idLocation);
 }
