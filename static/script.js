654f83065199a222f0b1c0890035081d104049aa
MarcoCarraro-1 <71838678+MarcoCarraro-1@users.noreply.github.com>
2020-11-23 16:14:16 +0100

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,4 +1,70 @@
 //here goes the script for the html page
+function myFunction(){
+    var x = document.getElementById("myFile");
+    var txt = "";
+    if ('files' in x) {
+        if (x.files.length == 0) {
+            txt = "Select one or more files.";
+        } else {
+            for (var i = 0; i < x.files.length; i++) {
+                txt += "<br><strong>" + (i+1) + ". file</strong><br>";
+                var file = x.files[i];
+                if((file.name.indexOf("jpg") != -1) || (file.name.indexOf("png") != -1)){
+                    if ('name' in file) {
+                        txt += "name: " + file.name + "<br>";
+                    }
+                    if ('size' in file) {
+                        txt += "size: " + file.size + " bytes <br>";
+                    }
+                } else {
+                    txt = "Formato non supportato! Inserire solo file .jpg o .png";
+                }
+            }
+        }
+    } else {
+        if (x.value == "") {
+            txt += "Select one or more files.";
+        } else {
+            txt += "The files property is not supported by your browser!";
+            txt  += "<br>The path of the selected file: " + x.value; // If the browser does not support the files property, it will return the path of the selected file instead. 
+        }
+    }
+    document.getElementById("demo").innerHTML = txt;
+    if(txt == "Formato non supportato! Inserire solo file .jpg o .png"){
+        x.value = "";
+    }
+}
+
+function addLocation(){
+    var nameLoc = document.getElementById("nome").value;
+    var addressLoc = document.getElementById("posizione").value;
+    var descLoc = document.getElementById("descrizione").value;
+    var imgLoc = document.getElementById("myFile");
+    var catLoc = "";
+    var likesLoc = 0;
+
+    console.log(nameLoc);
+
+    fetch('../locations', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify( { 
+            name: nameLoc,
+            address: addressLoc,
+            description: descLoc,
+            locationImage: imgLoc,
+            category: catLoc,
+            likes: likesLoc
+        } ),
+    })
+    .then((resp) => {
+        console.log(resp);
+        //loadBooks();
+        return;
+    })
+    .catch( error => console.error(error) ); // If there is any error you will catch them here
+
+};
 
 function loadLocations() {
 
