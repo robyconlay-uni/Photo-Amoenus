949e5389d1e3baf92610387c7e95274a933e9d86
Giovanni <giovanni.mistura@studenti.unitn.it>
2020-12-07 10:39:33 +0100

    adjust script.js for registration

--- static/script.js
+++ static/script.js
@@ -67,26 +67,6 @@ function addLocation(){
 }
 
 
-function registration() {
-
-    //get the form object
-    var username = document.getElementById("regUser").value;
-    var emailuser = document.getElementById("regEmail").value;
-    var passworduser = document.getElementById("regPd").value;
-    var password2 = document.getElementById("regPdConf").value;
-
-
-    fetch('../lib/user/registration', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify( { 
-            user: username,
-            email: emailuser,
-            password: passworduser
-        } ),
-    })
-
-};
 
 function loadLocations() {
 
