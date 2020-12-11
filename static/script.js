9e8a35730af7fd7d4ab79978c1512218b7d62f24
bonmassarivan <ivan.bonmassar@protonmail.com>
2020-12-11 17:45:21 +0100

    style update to addDone addlocation

--- static/script.js
+++ static/script.js
@@ -320,7 +320,7 @@ fetch('../user/login', {
     setCookie("email",data.email,1);
     let mes = data.message;
     if (mes.localeCompare("Auth successful") == 0) {
-        document.write("<div id='center'><h1>Log in avvenuto con successo!</h1><br><a href='index.html'>Torna alla home</a></div>");
+        document.write("<head><link rel='stylesheet' type='text/css' href='stylesheet.css'></head><body><div id='center' class='popup'><h1>Log in avvenuto con successo!</h1><form action='index.html'><button class= 'registerbtn' type='submit'> Torna alla home page </button></form> </body>");
     } else if (mes.localeCompare("Auth failed") == 0) {
         document.write("<div id='center'><h1>Log in non riuscito!</h1><br><a href='login.html'>Torna al LogIn</a></div>");
     } else {
